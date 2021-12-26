cee0558443de449af869ec20084a884a4349a62a
ali turkaman <ali.turckaman81@gmail.com>
2021-12-26 10:10:32 -0800

    project created

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Container } from '@chakra-ui/layout';
+import { FC } from 'react';
 import './App.css';
 
-function App() {
+const App : FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxW="container.3xl" minH="100vh" bg="#f8fafd" >
+      landing page
+    </Container>
   );
 }
 
