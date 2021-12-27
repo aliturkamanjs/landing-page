9e5456a23c9187cd4029e7f241d4170b2e518590
matinturkaman <matinturkaman@gmail.com>
2021-12-27 05:36:20 -0800

    structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { Container } from '@chakra-ui/layout';
-import { FC } from 'react';
-import './App.css';
+import "./App.css";
+import { Container } from "@chakra-ui/layout";
+import { FC } from "react";
+import { Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
-const App : FC = () => {
+const App: FC = () => {
   return (
-    <Container maxW="container.3xl" minH="100vh" bg="#f8fafd" >
-      landing page
+    <Container maxW="container.3xl" p="0px">
+      <Flex h="100vh">
+        <Box
+          bg="#000"
+          opacity="31%"
+          flex={1}
+        ></Box>
+        <Box flex={1}></Box>
+      </Flex>
     </Container>
   );
-}
+};
 
 export default App;
